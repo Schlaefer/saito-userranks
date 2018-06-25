c262b78403505e2741465884240b141b2e4e5b81
Schlaefer <openmail+sourcecode@siezi.com>
2018-06-25 15:13:24 +0200

    ✓ phpcbf auto-fixer

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,18 +1,17 @@
 <?php
 
-	//= don't activate on CLI-tests
-	if (php_sapi_name() === 'cli') {
-		return;
-	}
+//= don't activate on CLI-tests
+if (php_sapi_name() === 'cli') {
+    return;
+}
 
-	//= load plugin settings
-	$settings = Saito\Plugin::loadConfig('Userranks');
+//= load plugin settings
+$settings = Saito\Plugin::loadConfig('Userranks');
 
-	//= create Userranks class
-	App::uses('Userranks', 'Userranks.Lib');
-	$Userranks = new Userranks($settings);
+//= create Userranks class
+App::uses('Userranks', 'Userranks.Lib');
+$Userranks = new Userranks($settings);
 
-	//= attach Userranks class to the Saito Event Manager
-	App::uses('SaitoEventManager', 'Lib/Saito/Event');
-	SaitoEventManager::getInstance()->attach($Userranks);
-
+//= attach Userranks class to the Saito Event Manager
+App::uses('SaitoEventManager', 'Lib/Saito/Event');
+SaitoEventManager::getInstance()->attach($Userranks);
--- Lib/Userranks.php
+++ Lib/Userranks.php
@@ -1,39 +1,42 @@
 <?php
 
-	App::uses('SaitoEventListener', 'Lib/Saito/Event');
+App::uses('SaitoEventListener', 'Lib/Saito/Event');
 
-	class Userranks implements SaitoEventListener {
+class Userranks implements SaitoEventListener
+{
 
-		/**
-		 * @var array plugin-config
-		 */
-		protected $_settings;
+    /**
+     * @var array plugin-config
+     */
+    protected $_settings;
 
-		public function __construct($settings) {
-			$this->_settings = $settings;
-		}
+    public function __construct($settings)
+    {
+        $this->_settings = $settings;
+    }
 
-		public function implementedSaitoEvents() {
-			return [
-				// event => local method to call on event
-				'Request.Saito.View.User.beforeFullProfile' => 'onUserranks'
-			];
-		}
+    public function implementedSaitoEvents()
+    {
+        return [
+            // event => local method to call on event
+            'Request.Saito.View.User.beforeFullProfile' => 'onUserranks'
+        ];
+    }
 
-		public function onUserranks($eventData) {
-			$user = $eventData['user'];
+    public function onUserranks($eventData)
+    {
+        $user = $eventData['user'];
 
-			foreach ($this->_settings['ranks'] as $treshold => $title) {
-				$rank = $title;
-				if ($user['User']['number_of_entries'] <= $treshold) {
-					break;
-				}
-			}
+        foreach ($this->_settings['ranks'] as $treshold => $title) {
+            $rank = $title;
+            if ($user['User']['number_of_entries'] <= $treshold) {
+                break;
+            }
+        }
 
-			return [
-				'title' => __d('userranks', 'rank'),
-				'content' => $rank
-			];
-		}
-
-	}
+        return [
+            'title' => __d('userranks', 'rank'),
+            'content' => $rank
+        ];
+    }
+}
